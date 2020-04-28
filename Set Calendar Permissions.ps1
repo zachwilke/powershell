c41b7b168ba6df737bb166b12067319be8f35119
zachwilke <45574453+zachwilke@users.noreply.github.com>
2020-04-28 09:24:55 -0500

    Update Set Calendar Permissions.ps1

--- Set Calendar Permissions.ps1
+++ Set Calendar Permissions.ps1
@@ -1,2 +1,2 @@
-﻿set-MailboxFolderPermission -Identity tarpon@fellowshipchurch.com:\calendar -user "katlin.swanson@fellowshipchurch.com" -AccessRights Editor
+﻿set-MailboxFolderPermission -Identity givingpermission@example.com:\calendar -user "gettingpermission@example.com" -AccessRights Editor
 #change set to add if you need to add a user who doesn't already have permissions. Set will edit an existing set of permissions.
